eb16ff65416b10236cc2b378fffd55147f36d8d7
Alex-Guha <alexandersguha@gmail.com>
2026-06-08 17:31:04 -0700

    Dropped references from deployed editor

--- diagram-app/main.js
+++ diagram-app/main.js
@@ -1,10 +1,8 @@
 import { createEditor } from '@alexguha/rplib-editor';
-import { referencesPanel } from './referencesPanel.js';
 
 window.onload = async () => {
     const manager = await createEditor({
         dataDir: './data',
-        panels: [referencesPanel],
     });
     manager.restoreView('test');
 };
